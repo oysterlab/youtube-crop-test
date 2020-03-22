0f6de879cc1af6dee30fc3a405d254da9f1df13a
oysterlab <oysterlaboratory@gmail.com>
2020-03-22 18:51:58 +0900

    s

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -6,6 +6,16 @@ document.body.append(webview)
 webview.src = 'https://m.youtube.com/watch?v=g0U95UErodo'
 
 webview.addEventListener('dom-ready', () => {
+  setTimeout(() => {
+    webview.style.width = '640px'
+    webview.style.height = '480px'
+    console.log('resized 640px')    
+    setTimeout(() => {
+      webview.style.width = '1280px'
+      webview.style.height = '720px'    
+      console.log('resized 1280px')          
+    }, 5000)
+  }, 1000)
   console.log(webview)
   webview.executeJavaScript(`
   document.addEventListener("webkitfullscreenchange", function(event) {
@@ -16,6 +26,8 @@ webview.addEventListener('dom-ready', () => {
     console.log(document.webkitFullscreenElement)
     if (document.webkitFullscreenElement) { //full screen
       setTimeout(() => {
+        // webview.style.width = '1024px'
+        // webview.style.height = '720px'
         const d = document.getElementById('player-theater-container')
         const videoRect = d.getClientRects()[0]
         const sx = videoRect.x
@@ -23,13 +35,12 @@ webview.addEventListener('dom-ready', () => {
         const sy = videoRect.y
         const ey = videoRect.y + videoRect.height
 
-        const clipStyle = 'polygon(' + sx + 'px ' + sy + 'px, ' + ex + 'px ' + sy + 'px, ' + ex + 'px ' + ey + 'px, ' + sx + 'px ' + ey + 'px)'
-        ytdEle.style.clipPath = clipStyle
+        // const clipStyle = 'polygon(' + sx + 'px ' + sy + 'px, ' + ex + 'px ' + sy + 'px, ' + ex + 'px ' + ey + 'px, ' + sx + 'px ' + ey + 'px)'
+        // ytdEle.style.clipPath = clipStyle
       }, 100)
     } else {
-      console.log('chekc')
       setTimeout(() => {
-        ytdEle.style.clipPath = ''
+        // ytdEle.style.clipPath = ''
       }, 100)
     }
     // event.stopPropagation()
